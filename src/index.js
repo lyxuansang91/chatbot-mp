f8017c9b650f937fb45d814b1cee06c4a67a13b1
Son Pham <pxson.2903@gmail.com>
2019-07-23 16:07:20 +0700

    Add Cron

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ const { port, env } = require('./config/vars');
 const logger = require('./config/logger');
 const app = require('./config/express');
 const mongoose = require('./config/mongoose');
+const cron = require('node-cron');
 
 // open mongoose connection
 mongoose.connect();
 
 // listen to requests
-app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+app.listen(port, () => {
+    logger.info(`server started on port ${port} (${env})`);
+
+    cron.schedule('0 0 0 * * *', () => {
+        console.log('running update every day at 12:00 AM');
+    });
+});
 
 /**
 * Exports express
