f049a5bd74f651a8470ebe4a93bcf9bfa0aea89a
Son Pham <pxson.2903@gmail.com>
2019-08-09 10:59:30 +0700

    Handle submit message API in multipart

--- src/api/controllers/message.controller.js
+++ src/api/controllers/message.controller.js
@@ -1,55 +1,105 @@
 /*eslint-disable */
 
-const httpStatus = require('http-status');
-const { omit } = require('lodash');
-var request = require('request');
+const httpStatus = require("http-status");
+const { omit } = require("lodash");
+var request = require("request");
+const axios = require("axios");
+const shortid = require("shortid");
+const path = require("path");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
-const ZaloClient = require('../services/zaloService').ZaloClient;
-const ZaloUser = require('../models/zalouser.model');
-const Message = require('../models/message.model');
+const ZaloClient = require("../services/zaloService").ZaloClient;
+const ZaloUser = require("../models/zalouser.model");
+const Message = require("../models/message.model");
+
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "./uploads/",
+    filename: function(req, file, cb) {
+      // user shortid.generate() alone if no extension is needed
+      cb(null, shortid.generate() + path.parse(file.originalname).ext);
+    }
+  })
+}).single("file");
 
 exports.send = async (req, res) => {
   try {
-    const {
-      user_ids: userIds,
-      type,
-      link,
-      message,
-      title: linktitle,
-      description: linkdes,
-      thumbnail: linkthumb,
-    } = req.body;
+    var form = new formidable.IncomingForm();
 
-    if (!userIds) {
-      const users = await ZaloUser.list({ page: 1, perPage: 100 });
+    // form.parse analyzes the incoming stream data, picking apart the different fields and files for you.
+    form.parse(req, async function(err, fields, files) {
+      if (err) {
+        // Check for and handle any errors here.
+        console.error(err.message);
+        return;
+      }
 
-      users.forEach(user => {
-        const uid = user.fromuid;
-        switch (type) {
-          case 'text':
-            sendTextMessage(uid, message);
-            break;
-          case 'text_link':
-            sendTextLink(uid, link, linktitle, linkdes, linkthumb);
-            break;
-        }
-      });
-    } else {
-      userIds.forEach(uid => {
-        switch (type) {
-          case 'text':
-            sendTextMessage(uid, message);
-            break;
-          case 'text_link':
-            sendTextLink(uid, link, linktitle, linkdes, linkthumb);
-            break;
-        }
-      });
-    }
+      let linkthumb = null
+      if (files.file) {
+        // handle upload file
+        const uploadedFileName = shortid.generate() + path.parse(files.file.name).ext
+        linkthumb =
+          (process.env.NODE_ENV === "development"
+            ? req.protocol + "://" + req.get("host")
+            : process.env.BASE_URL) +
+          "/uploads/" +
+          uploadedFileName;
 
-    res.json({ status: 'success' });
+        fs.rename(
+          files.file.path,
+          "./uploads/" + uploadedFileName,
+          function(err) {
+            if (err) next(err);
+            res.end();
+          }
+        );
+      }
+
+      const {
+        user_ids,
+        type,
+        link,
+        message,
+        title: linktitle,
+        description: linkdes
+      } = fields;
+
+      const userIds = _.uniq(JSON.parse(user_ids));
+
+      if (!userIds) {
+        const users = await ZaloUser.list({ page: 1, perPage: 100 });
+
+        users.forEach(user => {
+          const uid = user.fromuid;
+          switch (type) {
+            case "text":
+              sendTextMessage(uid, message);
+              break;
+            case "text_link":
+              sendTextLink(uid, link, linktitle, linkdes, linkthumb);
+              break;
+          }
+        });
+      } else {
+        userIds.forEach(uid => {
+          switch (type) {
+            case "text":
+              sendTextMessage(uid, message);
+              break;
+            case "text_link":
+              sendTextLink(uid, link, linktitle, linkdes, linkthumb);
+              break;
+          }
+        });
+      }
+    });
+
+    res.json({ status: "success" });
   } catch (error) {
-    res.json({ status: 'failed', message: error.message });
+    res.json({ status: "failed", message: error.message });
   }
 };
 
@@ -57,9 +107,9 @@ exports.uploadMedia = async (req, res) => {
   try {
     uploadMedia();
 
-    res.json({ status: 'success' });
+    res.json({ status: "success" });
   } catch (error) {
-    res.json({ status: 'failed', message: error.message });
+    res.json({ status: "failed", message: error.message });
   }
 };
 
@@ -72,27 +122,32 @@ exports.getMessageHistory = async (req, res) => {
     const messages = await Message.list({ page, perPage, fromuid, status });
     const transformedMessages = messages.map(message => message.transform());
 
-    res.json({ status: 'success', data: transformedMessages });
+    res.json({ status: "success", data: transformedMessages });
   } catch (error) {
-    res.json({ status: 'failed', message: error.message });
+    res.json({ status: "failed", message: error.message });
   }
 };
 
 uploadMedia = async () => {
-  var fileUrl = 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg';
-  ZaloClient.api('upload/image', 'POST', { file: fileUrl }, function(response) {});
+  var fileUrl =
+    "https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg";
+  ZaloClient.api("upload/image", "POST", { file: fileUrl }, function(
+    response
+  ) {});
 };
 
 sendTextMessage = async (uid, message) => {
-  ZaloClient.api('sendmessage/text', 'POST', { uid, message }, function(response) {
+  ZaloClient.api("sendmessage/text", "POST", { uid, message }, function(
+    response
+  ) {
     if (response.data && response.data.msgId) {
       const zaloMessageId = response.data.msgId;
       const data = {
         zaloMessageId,
         uid,
-        messageType: 'text',
+        messageType: "text",
         message,
-        status: response.errorMsg === 'Success' ? 'success' : 'failed',
+        status: response.errorMsg === "Success" ? "success" : "failed"
       };
       const msg = new Message(data);
       msg.save();
@@ -100,9 +155,9 @@ sendTextMessage = async (uid, message) => {
       const data = {
         zaloMessageId: null,
         uid,
-        messageType: 'text',
+        messageType: "text",
         message,
-        status: 'failed',
+        status: "failed"
       };
       const msg = new Message(data);
       msg.save();
@@ -117,21 +172,23 @@ sendTextLink = async (uid, link, linktitle, linkdes, linkthumb) => {
         link,
         linktitle,
         linkdes,
-        linkthumb,
-      },
-    ],
+        linkthumb
+      }
+    ]
   };
 
-  ZaloClient.api('sendmessage/links', 'POST', { uid, ...message }, function(response) {
-    console.log('response:', response);
+  ZaloClient.api("sendmessage/links", "POST", { uid, ...message }, function(
+    response
+  ) {
+    console.log("response:", response);
     if (response.data && response.data.msgId) {
       const zaloMessageId = response.data.msgId;
       const data = {
         zaloMessageId,
         uid,
-        messageType: 'text_link',
+        messageType: "text_link",
         message: JSON.stringify(message),
-        status: response.errorMsg === 'Success' ? 'success' : 'failed',
+        status: response.errorMsg === "Success" ? "success" : "failed"
       };
       const msg = new Message(data);
       msg.save();
@@ -139,9 +196,9 @@ sendTextLink = async (uid, link, linktitle, linkdes, linkthumb) => {
       const data = {
         zaloMessageId: null,
         uid,
-        messageType: 'text_link',
+        messageType: "text_link",
         message: JSON.stringify(message),
-        status: 'failed',
+        status: "failed"
       };
       const msg = new Message(data);
       msg.save();
@@ -154,20 +211,22 @@ sendInteractiveMessage = async uid => {
     uid: uid,
     actionlist: [
       {
-        action: 'oa.open.inapp',
-        title: 'Send interactive messages',
-        description: 'This is a test for API send interactive messages',
-        thumb: 'https://developers.zalo.me/web/static/prodution/images/bg.jpg',
-        href: 'https://developers.zalo.me',
-        data: 'https://developers.zalo.me',
+        action: "oa.open.inapp",
+        title: "Send interactive messages",
+        description: "This is a test for API send interactive messages",
+        thumb: "https://developers.zalo.me/web/static/prodution/images/bg.jpg",
+        href: "https://developers.zalo.me",
+        data: "https://developers.zalo.me",
         popup: {
-          title: 'Open Website Zalo For Developers',
-          desc: 'Click ok to visit Zalo For Developers and read more Document',
-          ok: 'ok',
-          cancel: 'cancel',
-        },
-      },
-    ],
+          title: "Open Website Zalo For Developers",
+          desc: "Click ok to visit Zalo For Developers and read more Document",
+          ok: "ok",
+          cancel: "cancel"
+        }
+      }
+    ]
   };
-  ZaloClient.api('sendmessage/actionlist', 'POST', params, function(response) {});
+  ZaloClient.api("sendmessage/actionlist", "POST", params, function(
+    response
+  ) {});
 };
