aa71441328092672d9c1c54e25eb931709494d86
Son Pham <pxson.2903@gmail.com>
2019-08-16 17:19:13 +0700

    Return sent message

--- src/api/controllers/message.controller.js
+++ src/api/controllers/message.controller.js
@@ -1,5 +1,4 @@
 /*eslint-disable */
-
 const httpStatus = require("http-status");
 const { omit } = require("lodash");
 var request = require("request");
@@ -14,17 +13,13 @@ const ZaloClient = require("../services/zaloService").ZaloClient;
 const ZaloUser = require("../models/zalouser.model");
 const Message = require("../models/message.model");
 
-
-
 exports.send = async (req, res) => {
   try {
-    var form = new formidable.IncomingForm();
-
     const thumbnail = req.file;
     const fields = req.body;
 
     console.log("req.fields", req.body);
-    console.log("========================")
+    console.log("========================");
     console.log("req.files", req.file);
     console.log("========================");
 
@@ -41,9 +36,7 @@ exports.send = async (req, res) => {
         "/uploads/" +
         uploadedFileName;
 
-      fs.rename(thumbnail.path, "./uploads/" + uploadedFileName, function(
-        err
-      ) {
+      fs.rename(thumbnail.path, "./uploads/" + uploadedFileName, function(err) {
         if (err) {
           console.error(err.message);
         }
@@ -59,63 +52,136 @@ exports.send = async (req, res) => {
       description: linkdes
     } = fields;
 
-    const userIds = user_ids ? _.uniq(JSON.parse(user_ids)) : []
+    const userIds = user_ids ? _.uniq(JSON.parse(user_ids)) : [];
 
     if (userIds.length === 0) {
-      const users = await ZaloUser.list({ page: 1, perPage: 100 });
+      const users = await ZaloUser.list({
+        page: 1,
+        perPage: 100
+      });
 
-      users.forEach(user => {
+      users.forEach(async user => {
+        let sentMessage = null;
         const uid = user.fromuid;
         switch (type) {
           case "text":
-            sendTextMessage(uid, message);
+            sentMessage = await sendTextMessage(uid, message);
             break;
           case "text_link":
-            sendTextLink(uid, link, linktitle, linkdes, linkthumb);
+            sentMessage = await sendTextLink(uid, link, linktitle, linkdes, linkthumb);
             break;
         }
 
-        console.log(
-          "Send message:",
-          JSON.stringify({
-            user_ids: uid,
-            type,
-            link,
-            title: linktitle,
-            description: linkdes,
-            thumbnail: linkthumb
-          })
-        );
+        console.log("Sent message:", JSON.stringify(sentMessage));
       });
     } else {
-      userIds.forEach(uid => {
+      userIds.forEach(async uid => {
+        let sentMessage = null;
+
         switch (type) {
           case "text":
-            sendTextMessage(uid, message);
+            sentMessage = await sendTextMessage(uid, message);
             break;
           case "text_link":
-            sendTextLink(uid, link, linktitle, linkdes, linkthumb);
+            sentMessage = await sendTextLink(uid, link, linktitle, linkdes, linkthumb);
             break;
         }
 
         console.log(
-          "Send message:",
-          JSON.stringify({
-            user_ids: uid,
-            type,
-            link,
-            title: linktitle,
-            description: linkdes,
-            thumbnail: linkthumb
-          })
+          "Sent message:",
+          JSON.stringify(sentMessage)
         );
       });
     }
 
-    res.json({ status: "success" });
+    res.json({
+      status: "success"
+    });
   } catch (error) {
-    console.log('error', error);
-    res.json({ status: "failed", message: error.message });
+    console.log("error", error);
+    res.json({
+      status: "failed",
+      message: error.message
+    });
+  }
+};
+
+exports.sendToUser = async (req, res) => {
+  try {
+    const thumbnail = req.file;
+    const fields = req.body;
+
+    console.log("req.fields", req.body);
+    console.log("========================");
+    console.log("req.files", req.file);
+    console.log("========================");
+
+    // form.parse analyzes the incoming stream data, picking apart the different fields and files for you.
+    let linkthumb = null;
+    if (thumbnail) {
+      // handle upload file
+      const uploadedFileName =
+        shortid.generate() + path.parse(thumbnail.originalname).ext;
+      linkthumb =
+        (process.env.NODE_ENV === "development"
+          ? req.protocol + "://" + req.get("host")
+          : process.env.BASE_URL) +
+        "/uploads/" +
+        uploadedFileName;
+
+      fs.rename(thumbnail.path, "./uploads/" + uploadedFileName, function(err) {
+        if (err) {
+          console.error(err.message);
+        }
+      });
+    }
+
+    const {
+      type,
+      link,
+      message,
+      title: linktitle,
+      description: linkdes
+    } = fields;
+
+    const uid = req.params.id;
+
+    let sentMessage = null;
+
+    if (uid) {
+      switch (type) {
+        case "text":
+          sentMessage = await sendTextMessage(uid, message);
+          break;
+        case "text_link":
+          sentMessage = await sendTextLink(
+            uid,
+            link,
+            linktitle,
+            linkdes,
+            linkthumb
+          );
+          break;
+      }
+      console.log("Sent message:", JSON.stringify({ sentMessage }));
+
+      res.json({
+        status: "success",
+        message: sentMessage ? sentMessage.transform() : null
+      });
+    } else {
+      res.json({
+        status: "failed",
+        message: "Không thể gửi tin nhắn!"
+      });
+    }
+
+  } catch (error) {
+    console.log("error", error);
+    res.json({
+      status: "failed",
+      message: error.message
+    });
   }
 };
 
@@ -123,9 +189,14 @@ exports.uploadMedia = async (req, res) => {
   try {
     uploadMedia();
 
-    res.json({ status: "success" });
+    res.json({
+      status: "success"
+    });
   } catch (error) {
-    res.json({ status: "failed", message: error.message });
+    res.json({
+      status: "failed",
+      message: error.message
+    });
   }
 };
 
@@ -135,50 +206,68 @@ exports.getMessageHistory = async (req, res) => {
     page = page ? Number(page) : 1;
     perPage = perPage ? Number(perPage) : 100;
 
-    const messages = await Message.list({ page, perPage, uid, status });
+    const messages = await Message.list({
+      page,
+      perPage,
+      uid,
+      status
+    });
     const transformedMessages = messages.map(message => message.transform());
 
-    res.json({ status: "success", data: transformedMessages });
+    res.json({
+      status: "success",
+      data: transformedMessages
+    });
   } catch (error) {
-    res.json({ status: "failed", message: error.message });
+    res.json({
+      status: "failed",
+      message: error.message
+    });
   }
 };
 
 uploadMedia = async () => {
   var fileUrl =
     "https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg";
-  ZaloClient.api("upload/image", "POST", { file: fileUrl }, function(
-    response
-  ) {});
+  ZaloClient.api(
+    "upload/image",
+    "POST",
+    {
+      file: fileUrl
+    },
+    function(response) {}
+  );
 };
 
 sendTextMessage = async (uid, message) => {
-  ZaloClient.api("sendmessage/text", "POST", { uid, message }, function(
-    response
-  ) {
-    if (response.data && response.data.msgId) {
-      const zaloMessageId = response.data.msgId;
-      const data = {
-        zaloMessageId,
-        uid,
-        messageType: "text",
-        message,
-        status: response.errorMsg === "Success" ? "success" : "failed"
-      };
-      const msg = new Message(data);
-      msg.save();
-    } else {
-      const data = {
-        zaloMessageId: null,
-        uid,
-        messageType: "text",
-        message,
-        status: "failed"
-      };
-      const msg = new Message(data);
-      msg.save();
-    }
+  const response = await ZaloClient.api("sendmessage/text", "POST", {
+    uid,
+    message
   });
+  if (response.data && response.data.msgId) {
+    const zaloMessageId = response.data.msgId;
+    const data = {
+      zaloMessageId,
+      uid,
+      messageType: "text",
+      message,
+      status: response.errorMsg === "Success" ? "success" : "failed"
+    };
+    const msg = new Message(data);
+    await msg.save();
+    return msg;
+  } else {
+    const data = {
+      zaloMessageId: null,
+      uid,
+      messageType: "text",
+      message,
+      status: "failed"
+    };
+    const msg = new Message(data);
+    await msg.save();
+    return msg;
+  }
 };
 
 sendTextLink = async (uid, link, linktitle, linkdes, linkthumb) => {
@@ -193,33 +282,34 @@ sendTextLink = async (uid, link, linktitle, linkdes, linkthumb) => {
     ]
   };
 
-  ZaloClient.api("sendmessage/links", "POST", { uid, ...message }, function(
-    response
-  ) {
-    console.log("response:", response);
-    if (response.data && response.data.msgId) {
-      const zaloMessageId = response.data.msgId;
-      const data = {
-        zaloMessageId,
-        uid,
-        messageType: "text_link",
-        message: JSON.stringify(message),
-        status: response.errorMsg === "Success" ? "success" : "failed"
-      };
-      const msg = new Message(data);
-      msg.save();
-    } else {
-      const data = {
-        zaloMessageId: null,
-        uid,
-        messageType: "text_link",
-        message: JSON.stringify(message),
-        status: "failed"
-      };
-      const msg = new Message(data);
-      msg.save();
-    }
+  const response = await ZaloClient.api("sendmessage/links", "POST", {
+    uid,
+    ...message
   });
+  if (response.data && response.data.msgId) {
+    const zaloMessageId = response.data.msgId;
+    const data = {
+      zaloMessageId,
+      uid,
+      messageType: "text_link",
+      message: JSON.stringify(message),
+      status: response.errorMsg === "Success" ? "success" : "failed"
+    };
+    const msg = new Message(data);
+    await msg.save();
+    return msg;
+  } else {
+    const data = {
+      zaloMessageId: null,
+      uid,
+      messageType: "text_link",
+      message: JSON.stringify(message),
+      status: "failed"
+    };
+    const msg = new Message(data);
+    await msg.save();
+    return msg;
+  }
 };
 
 // sendInteractiveMessage = async uid => {
--- src/api/routes/v1/message.route.js
+++ src/api/routes/v1/message.route.js
@@ -72,7 +72,8 @@ router
    * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
    * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
    */
-  .post("/", authorize(ADMIN), upload, controller.send);
+  .post("/", authorize(ADMIN), upload, controller.send)
+  .post("/:id", authorize(ADMIN), upload, controller.sendToUser);
 
 router.post("/media", authorize(ADMIN), controller.uploadMedia);
 
